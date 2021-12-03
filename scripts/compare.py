d36c2758ab735efd7442b9793ad136545def4e06
mabdullahsoyturk <msoyturk20@ku.edu.tr>
2021-12-03 16:34:06 +0300

    update compare script

--- scripts/compare.py
+++ scripts/compare.py
@@ -1,32 +1,21 @@
-results1 = []
-results2 = []
-
-iter_num = 0
+results1, results2 = [], []
 
 with open("../results/test_single.txt") as result_single:
     lines = result_single.readlines()
 
     for line in lines:
         left, right = line.split(":")
-        if left != "Iteration":
-            results1.append((left, right, iter_num))
-        else:
-            iter_num = int(right)
+        results1.append((left, right))
 
 with open("../results/test_version5.txt") as result_version1:
     lines = result_version1.readlines()
     
     for line in lines:
         left, right = line.split(":")
-        if left != "Iteration":
-            results2.append((left, right, iter_num))
-        else:
-            iter_num = int(right.strip())
+        results2.append((left, right))
 
 for result1, result2 in zip(results1, results2):
     if result1[1] != result2[1]:
         print("Different!!!")
         print(f'Result1 {result1[0]}:{result1[1]}Result2 {result2[0]}:{result2[1]}')
         break
-        
-
